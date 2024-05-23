d6237c58df563229bf7944a1ee5692768305dec9
Julian Cadieux <julian.cadieux@stu.ocsb.ca>
2024-05-23 09:52:28 -0400

    latest

--- script.js
+++ script.js
@@ -13,10 +13,15 @@
 document.getElementById("over").addEventListener("mouseover", over)
 document.getElementById("enter").addEventListener("mouseenter", enter)
 document.getElementById("move").addEventListener("mousemove", move)
+document.getElementById("btn_show_hide").addEventListener("click", peakaboo)
+document.getElementById("door").addEventListener("click", openClose)
+// keypress
+window.addEventListener("keydown", keydown)
 
 let mouseover_count = 0;
 let mouseenter_count = 0;
 let mousemove_count = 0;
+let doorOpen = false
 
 function over() {
   document.getElementById("over_span").innerText = ++mouseover_count;
@@ -28,4 +33,22 @@ function enter() {
 
 function move() {
   document.getElementById("move_span").innerText = ++mousemove_count;
+}
+
+function peakaboo() {
+  document.getElementById("hidden_text").hidden = !document.getElementById("hidden_text").hidden
+}
+
+function openClose() {
+  if (doorOpen) {
+    document.getElementById("door").src = "images/door_closed.png"
+  } else {
+    document.getElementById("door").src = "images/door_open.png"
+  }
+  doorOpen = !doorOpen  
+}
+
+function keydown(event) {
+  console.log(event.key)
+  event.preventDefault()
 }
